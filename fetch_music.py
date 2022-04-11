1c7d740b28a18390a9c067f3d1673c009020f8b0
fuxiaofang <fuxiaofang93@gmail.com>
2022-04-11 22:27:50 +0800

    循环下载移动文件

--- fetch_music.py
+++ fetch_music.py
@@ -16,10 +16,7 @@ from europe_new_songs import europe_new_songs
 from europe_top import europe_songs
 from farm_songs import farm_songs
 from hongkong_top import hongkong_top
-from hot_songs import new_songs
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from hot_songs import new_songs, hot_songs
 
 from japan_top import japan_top
 from old_school_songs import old_school_songs
@@ -30,17 +27,36 @@ from speed_up_songs import speed_up_songs
 
 
 def download_music():
+    music_sets = {
+        'china_golden': china_golden,
+        'china_top': china_top,
+        'create_top': create_top,
+        'europe_new_songs': europe_new_songs,
+        'europe_top': europe_songs,
+        'farm_songs': farm_songs,
+        'hongkong_top': hongkong_top,
+        'hot_songs': hot_songs,
+        'new_songs': new_songs,
+        'japan_top': japan_top,
+        'old_school_songs': old_school_songs,
+        'rock_songs': rock_songs,
+        'south_korea_top': south_korea_top,
+        'speak_songs': speak_songs,
+        'speed_up_songs': speed_up_songs
+    }
+    for key in music_sets:
+        print(key + ':')
+        os.system('mkdir ' + key)
+        down_task(key, music_sets[key])
+
+
+def down_task(key, value):
     url = 'https://api.zhuolin.wang/api.php?callback=jQuery111305275055645768552_1649501603997&types=url&id='
     urlTail = '&source=netease&_=1649501604001'
-    singleSong = 'jQuery111305275055645768552_1649501603997({"url":"https:\/\/m701.music.126.net\/20220409193638\/7d420735903e072e05bf934b9177e223\/jdymusic\/obj\/wo3DlMOGwrbDjj7DisKw\/11983356173\/ed2f\/6024\/be41\/2dc456563c5f9c9535b75ecb066c0325.mp3","size":4096941,"br":128.001})'
-    bean = singleSong.split('(')[1].split(')')[0]
-    print(bean)
-    board = json.loads(south_korea_top)['playlist']
+    board = json.loads(value)['playlist']
     tracks = board['tracks']
     print(len(tracks))
     for index, item in enumerate(tracks):
-        # if index < 100:
-        #     continue
         print('下载：' + str(item['name']) + ' : ' + str(item['id']))
         querySongUrl = url + str(item['id']) + urlTail
         print(querySongUrl)
@@ -59,6 +75,8 @@ def download_music():
         response = requests.get(download_url)
         open(str(item['name']).replace('/', '') + ".mp3", "wb").write(response.content)
         time.sleep(1)
+    os.system('mv *.mp3 ' + key)
+
 
 
 if __name__ == '__main__':
